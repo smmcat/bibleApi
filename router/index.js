7ce0b1433fdc695dc9cc345eee4ef610e88d17fa
smmcat <1365116009@qq.com>
2023-10-12 10:21:48 +0800

    完善 /chapter 路由功能

--- router/index.js
+++ router/index.js
@@ -129,77 +129,102 @@ router.get('/bible', function (req, res) {
 
 
 router.get('/chapter', function (req, res) {
-    res.send({
-        status: 200,
-        data: [
-            { shortName: 'gn', lection: '创世纪', chapter: 50 },
-            { shortName: 'ex', lection: '出埃及记', chapter: 40 },
-            { shortName: 'lv', lection: '利未记', chapter: 27 },
-            { shortName: 'nm', lection: '民数记', chapter: 36 },
-            { shortName: 'dt', lection: '申命记', chapter: 34 },
-            { shortName: 'js', lection: '约书亚记', chapter: 24 },
-            { shortName: 'jud', lection: '士师记', chapter: 21 },
-            { shortName: 'rt', lection: '路得记', chapter: 4 },
-            { shortName: '1sm', lection: '撒母耳记上', chapter: 31 },
-            { shortName: '2sm', lection: '撒母耳记下', chapter: 24 },
-            { shortName: '1kgs', lection: '列王纪上', chapter: 22 },
-            { shortName: '2kgs', lection: '列王纪下', chapter: 25 },
-            { shortName: '1ch', lection: '历代志上', chapter: 29 },
-            { shortName: '2ch', lection: '历代志下', chapter: 36 },
-            { shortName: 'ezr', lection: '以斯拉记', chapter: 10 },
-            { shortName: 'ne', lection: '尼西米记', chapter: 13 },
-            { shortName: 'et', lection: '以斯帖记', chapter: 10 },
-            { shortName: 'job', lection: '约伯记', chapter: 42 },
-            { shortName: 'ps', lection: '诗篇', chapter: 150 },
-            { shortName: 'prv', lection: '箴言', chapter: 31 },
-            { shortName: 'ec', lection: '传道书', chapter: 12 },
-            { shortName: 'so', lection: '雅歌', chapter: 8 },
-            { shortName: 'is', lection: '以赛亚书', chapter: 66 },
-            { shortName: 'jr', lection: '耶利米书', chapter: 52 },
-            { shortName: 'lm', lection: '耶利米哀歌', chapter: 5 },
-            { shortName: 'ez', lection: '以西结书', chapter: 48 },
-            { shortName: 'dn', lection: '但以理书', chapter: 12 },
-            { shortName: 'ho', lection: '何西阿书', chapter: 14 },
-            { shortName: 'jl', lection: '约珥书', chapter: 3 },
-            { shortName: 'am', lection: '阿摩司书', chapter: 9 },
-            { shortName: 'ob', lection: '俄巴底亚书', chapter: 1 },
-            { shortName: 'jn', lection: '约拿书', chapter: 4 },
-            { shortName: 'mi', lection: '弥迦书', chapter: 7 },
-            { shortName: 'na', lection: '那鸿书', chapter: 3 },
-            { shortName: 'hk', lection: '哈巴谷书', chapter: 3 },
-            { shortName: 'zp', lection: '西番雅书', chapter: 3 },
-            { shortName: 'hg', lection: '哈该书', chapter: 2 },
-            { shortName: 'zc', lection: '撒迦利亚书', chapter: 14 },
-            { shortName: 'ml', lection: '玛拉基书', chapter: 4 },
-            { shortName: 'mt', lection: '马太福音', chapter: 28 },
-            { shortName: 'mk', lection: '马可福音', chapter: 16 },
-            { shortName: 'lk', lection: '路加福音', chapter: 24 },
-            { shortName: 'jo', lection: '约翰福音', chapter: 21 },
-            { shortName: 'act', lection: '使徒行传', chapter: 28 },
-            { shortName: 'rm', lection: '罗马书', chapter: 16 },
-            { shortName: '1co', lection: '哥林多前书', chapter: 16 },
-            { shortName: '2co', lection: '哥林多后书', chapter: 13 },
-            { shortName: 'gl', lection: '加拉太书', chapter: 6 },
-            { shortName: 'eph', lection: '以弗所书', chapter: 6 },
-            { shortName: 'ph', lection: '腓利比书', chapter: 4 },
-            { shortName: 'cl', lection: '歌罗西书', chapter: 4 },
-            { shortName: '1ts', lection: '帖撒罗尼迦前书', chapter: 5 },
-            { shortName: '2ts', lection: '帖撒罗尼迦后书', chapter: 3 },
-            { shortName: '1tm', lection: '提摩太前书', chapter: 6 },
-            { shortName: '2tm', lection: '提摩太后书', chapter: 4 },
-            { shortName: 'tt', lection: '提多书', chapter: 3 },
-            { shortName: 'phm', lection: '腓利门书', chapter: 1 },
-            { shortName: 'hb', lection: '希伯来书', chapter: 13 },
-            { shortName: 'jm', lection: '雅各书', chapter: 5 },
-            { shortName: '1pe', lection: '彼得前书', chapter: 5 },
-            { shortName: '2pe', lection: '彼得后书', chapter: 3 },
-            { shortName: '1jo', lection: '约翰一书', chapter: 5 },
-            { shortName: '2jo', lection: '约翰二书', chapter: 1 },
-            { shortName: '3jo', lection: '约翰三书', chapter: 1 },
-            { shortName: 'jd', lection: '犹大书', chapter: 1 },
-            { shortName: 're', lection: '启示录', chapter: 22 }
-        ]
-    })
+    // 圣经章节数据
+    const chapterData = [
+        { shortName: 'gn', lection: '创世记', chapter: 50 },
+        { shortName: 'ex', lection: '出埃及记', chapter: 40 },
+        { shortName: 'lv', lection: '利未记', chapter: 27 },
+        { shortName: 'nm', lection: '民数记', chapter: 36 },
+        { shortName: 'dt', lection: '申命记', chapter: 34 },
+        { shortName: 'js', lection: '约书亚记', chapter: 24 },
+        { shortName: 'jud', lection: '士师记', chapter: 21 },
+        { shortName: 'rt', lection: '路得记', chapter: 4 },
+        { shortName: '1sm', lection: '撒母耳记上', chapter: 31 },
+        { shortName: '2sm', lection: '撒母耳记下', chapter: 24 },
+        { shortName: '1kgs', lection: '列王纪上', chapter: 22 },
+        { shortName: '2kgs', lection: '列王纪下', chapter: 25 },
+        { shortName: '1ch', lection: '历代志上', chapter: 29 },
+        { shortName: '2ch', lection: '历代志下', chapter: 36 },
+        { shortName: 'ezr', lection: '以斯拉记', chapter: 10 },
+        { shortName: 'ne', lection: '尼西米记', chapter: 13 },
+        { shortName: 'et', lection: '以斯帖记', chapter: 10 },
+        { shortName: 'job', lection: '约伯记', chapter: 42 },
+        { shortName: 'ps', lection: '诗篇', chapter: 150 },
+        { shortName: 'prv', lection: '箴言', chapter: 31 },
+        { shortName: 'ec', lection: '传道书', chapter: 12 },
+        { shortName: 'so', lection: '雅歌', chapter: 8 },
+        { shortName: 'is', lection: '以赛亚书', chapter: 66 },
+        { shortName: 'jr', lection: '耶利米书', chapter: 52 },
+        { shortName: 'lm', lection: '耶利米哀歌', chapter: 5 },
+        { shortName: 'ez', lection: '以西结书', chapter: 48 },
+        { shortName: 'dn', lection: '但以理书', chapter: 12 },
+        { shortName: 'ho', lection: '何西阿书', chapter: 14 },
+        { shortName: 'jl', lection: '约珥书', chapter: 3 },
+        { shortName: 'am', lection: '阿摩司书', chapter: 9 },
+        { shortName: 'ob', lection: '俄巴底亚书', chapter: 1 },
+        { shortName: 'jn', lection: '约拿书', chapter: 4 },
+        { shortName: 'mi', lection: '弥迦书', chapter: 7 },
+        { shortName: 'na', lection: '那鸿书', chapter: 3 },
+        { shortName: 'hk', lection: '哈巴谷书', chapter: 3 },
+        { shortName: 'zp', lection: '西番雅书', chapter: 3 },
+        { shortName: 'hg', lection: '哈该书', chapter: 2 },
+        { shortName: 'zc', lection: '撒迦利亚书', chapter: 14 },
+        { shortName: 'ml', lection: '玛拉基书', chapter: 4 },
+        { shortName: 'mt', lection: '马太福音', chapter: 28 },
+        { shortName: 'mk', lection: '马可福音', chapter: 16 },
+        { shortName: 'lk', lection: '路加福音', chapter: 24 },
+        { shortName: 'jo', lection: '约翰福音', chapter: 21 },
+        { shortName: 'act', lection: '使徒行传', chapter: 28 },
+        { shortName: 'rm', lection: '罗马书', chapter: 16 },
+        { shortName: '1co', lection: '哥林多前书', chapter: 16 },
+        { shortName: '2co', lection: '哥林多后书', chapter: 13 },
+        { shortName: 'gl', lection: '加拉太书', chapter: 6 },
+        { shortName: 'eph', lection: '以弗所书', chapter: 6 },
+        { shortName: 'ph', lection: '腓利比书', chapter: 4 },
+        { shortName: 'cl', lection: '歌罗西书', chapter: 4 },
+        { shortName: '1ts', lection: '帖撒罗尼迦前书', chapter: 5 },
+        { shortName: '2ts', lection: '帖撒罗尼迦后书', chapter: 3 },
+        { shortName: '1tm', lection: '提摩太前书', chapter: 6 },
+        { shortName: '2tm', lection: '提摩太后书', chapter: 4 },
+        { shortName: 'tt', lection: '提多书', chapter: 3 },
+        { shortName: 'phm', lection: '腓利门书', chapter: 1 },
+        { shortName: 'hb', lection: '希伯来书', chapter: 13 },
+        { shortName: 'jm', lection: '雅各书', chapter: 5 },
+        { shortName: '1pe', lection: '彼得前书', chapter: 5 },
+        { shortName: '2pe', lection: '彼得后书', chapter: 3 },
+        { shortName: '1jo', lection: '约翰一书', chapter: 5 },
+        { shortName: '2jo', lection: '约翰二书', chapter: 1 },
+        { shortName: '3jo', lection: '约翰三书', chapter: 1 },
+        { shortName: 'jd', lection: '犹大书', chapter: 1 },
+        { shortName: 're', lection: '启示录', chapter: 22 }
+    ]
+    const shortName = req.query.shortName || false;
+    const lection = req.query.lection || false;
+    if (shortName) {
+        const index = chapterData.findIndex(item => item.shortName == shortName);
+        if(index == -1){
+            res.status(400).send({error:'未找到对应数据'})
+        }
+        res.send({
+            status:200,
+            data:chapterData[index]
+        })
+    } else if (lection) {
+        const index = chapterData.findIndex(item => item.lection == lection);
+        if(index == -1){
+            res.status(400).send({error:'超出或未找到对应章节'})
+        }
+        res.send({
+            status:200,
+            data:chapterData[index]
+        })
+    } else {
+        res.send({
+            status: 200,
+            data: chapterData
+        })
+    }
+
 })
 
 module.exports = router;
